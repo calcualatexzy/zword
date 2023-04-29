aeefd46dbbfb386242c23bd78757e4b748b79451
calcualatexzy <1712992409@qq.com>
2023-04-29 22:41:15 +0800

    new widget qt

--- zword/main.cpp
+++ zword/main.cpp
@@ -1,5 +1,5 @@
 
-#include "mainwindow.h"
+#include "widget.h"
 
 #include <QApplication>
 
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    Widget w;
     w.show();
     return a.exec();
 }
